2b20425414f5a92f699c13fb8276c14f3e661e62
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-09 12:46:38 +0530

    Longest Substring Without Repeating Characters

---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    internal class LongestSubstring
+    {
+        public int FindLongestSubstringWithoutRepeatingCharacters(string input)
+        {
+            int maxLen = 0;
+            HashSet<char> result = new HashSet<char>();
+            int i = 0;
+            int j = 0;
+
+            if (input.Length < 1)
+            {
+                return 1;
+            }
+
+            while (i < input.Length)
+            {
+                if (!result.Contains(input[i]))
+                {
+                    result.Add(input[i]);
+                    i++;
+                    maxLen = Math.Max(result.Count, maxLen);
+                }
+                else
+                {
+                    result.Remove(input[j]);
+                    j++;
+                }
+             }
+            return maxLen;
+        }
+            
+        
+    }
+}
---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LongestSubstringWithoutRepeatingCharacters;
+
+Console.WriteLine("Find Longest Substring with non-repeating caharcters");
+
+LongestSubstring obj = new LongestSubstring();
+
+Console.WriteLine($"Max length: {obj.FindLongestSubstringWithoutRepeatingCharacters("bbbbb")}");
+Console.ReadKey();
